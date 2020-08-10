334b268eea1d4212f245ce29bcb2c518a0f5fd0e
AbiCollopy <abigail.collopy.15@ucl.ac.uk>
2020-08-10 17:32:34 +0100

    Adding split second half of R code for plot 2, accidentally missed in first commit.

--- plot2.R
+++ plot2.R
@@ -13,3 +13,18 @@ data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 ##Subset data
 
 DT <- subset(data, subset = (data$Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+## Convert dates and times
+
+DT$datetime <- strptime(paste(DT$Date, DT$Time), 
+                        "%Y-%m-%d %H:%M:%S")
+
+DT$datetime <- as.POSIXct(DT$datetime)
+
+png("plot2.png", width = 480, height = 480)
+
+plot(DT$Global_active_power ~ DT$datetime, type = "l",
+     ylab = "Global Active Power (kilowatts)", 
+     xlab = "")
+
+dev.off()
